1db3dcb81c6fcf03d18f62277e280e49abafe397
Paulo <paulo.juchimiuk@alobebe.com.br>
2021-02-26 17:19:34 -0300

    correction method associate

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -1,7 +1,21 @@
 const Address = require('../model/Address');
 const User = require('../model/User');
+const { index } = require('./UserController');
 
 module.exports = {
+
+    async index(req, res){
+        const { user_id } = req.params;
+
+        const user = await User.findByPk(user_id);
+
+        const addresses = await Address.findAll({ where: {user_id}})
+
+        return res.json(addresses);
+    },
+
+
+
     async store(req, res){
         const { user_id } = req.params;
         const { zipcode, street, number } = req.body;
