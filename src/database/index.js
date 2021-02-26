ad07cfd984cbe15c45c67a8ff01f6886a012105c
Paulo <paulo.juchimiuk@alobebe.com.br>
2021-02-26 15:23:00 -0300

    add project

---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/database');
+
+const User = require('../model/User');
+const Address = require('../model/Address');
+
+const connection = new Sequelize(dbConfig);
+
+User.init(connection);
+Address.init(connection);
+
+module.exports = connection;
